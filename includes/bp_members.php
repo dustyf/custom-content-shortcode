d60f044143665a31b926264403187a2fd4777448
Dustin Filippini <dusty@dustyf.com>
2014-11-18 15:57:32 -0600

    Updating some formatting to WP Standards

--- includes/bp_members.php
+++ includes/bp_members.php
@@ -14,12 +14,12 @@ class CCS_BP_Member {
 
 	function __construct() {
 
-		add_shortcode('bp_members', array($this, 'members_shortcode'));
-		add_shortcode('bp_member', array($this, 'member_shortcode'));
+		add_shortcode( 'bp_members', array( $this, 'members_shortcode' ) );
+		add_shortcode( 'bp_member', array( $this, 'member_shortcode' ) );
 
 		self::$state['is_users_loop'] = false;
 
-		add_shortcode('blog', array($this, 'blog_shortcode'));
+		add_shortcode( 'blog', array( $this, 'blog_shortcode' ) );
 	}
 
 
@@ -44,26 +44,34 @@ class CCS_BP_Member {
 		$args = array();
 
 		// Just pass these
-		$pass_args = array('orderby','search','number','offset');
+		$pass_args = array( 'orderby', 'search', 'number', 'offset' );
 
 		foreach ($pass_args as $arg) {
-			if (isset($atts[$arg]))
+			if ( isset( $atts[$arg] ) )
 				$args[$arg] = $atts[$arg];
 		}
 
-		if (isset($atts['type']))
-			$args['type'] = strtolower($atts['type']);
-		if (isset($atts['include']))
-			$args['include'] = CCS_Loop::explode_list($atts['include']);
-		if (isset($atts['exclude']))
-			$args['exclude'] = CCS_Loop::explode_list($atts['exclude']);
+		if ( isset( $atts['type'] ) ) {
+			$args['type'] = strtolower( $atts['type'] );
+		}
+		if ( isset( $atts['include'] ) ) {
+			$args['include'] = CCS_Loop::explode_list( $atts['include'] );
+		}
+		if ( isset( $atts['exclude'] ) ) {
+			$args['exclude'] = CCS_Loop::explode_list( $atts['exclude'] );
+		}
 
-		if (isset($atts['search_terms']))
+		if (isset( $atts['search_terms'] ) ) {
 			$args['search_terms'] = esc_html( $atts['search_terms'] );
+		}
 
-		if (isset($atts['field']) && isset($atts['value'])) {
+		if ( isset( $atts['field'] ) && isset( $atts['value'] ) ) {
 			$args['meta_key'] = $atts['field'];
 			$args['meta_value'] = $atts['value'];
+		}
+
+		if ( isset( $atts['profile_fields'] ) ) {
+
 		}
 
 		$users = bp_core_get_users( $args );
@@ -82,7 +90,7 @@ class CCS_BP_Member {
 		}
 
 		self::$state['is_users_loop'] = false;
-		return implode('', $outputs);
+		return implode( '', $outputs );
 	}
 
 
@@ -106,14 +114,16 @@ class CCS_BP_Member {
 			self::$state['current_user_object'] = $current_user;
 		}
 
-		extract(shortcode_atts(array(
+		extract( shortcode_atts( array(
 			'field'            => '',
 			'meta'             => '', // Alias
 			'size'             => '',
 			'bp_profile_field' => '',
-		), $atts));
+		), $atts ) );
 
-		if(empty($current_user)) return; // no current user
+		if( empty( $current_user ) ) {
+			return;
+		} // no current user
 
 		// Get BuddyPress xprofile data
 		if ( ! empty( $bp_profile_field ) ) {
@@ -121,11 +131,11 @@ class CCS_BP_Member {
 		}
 
 		// Get field specified
-		if( !empty($meta) ) $field=$meta;
+		if( ! empty( $meta ) ) $field=$meta;
 
-		if ( empty($field) ) {
+		if ( empty( $field ) ) {
 			// or just get the first parameter
-			$field = isset($atts[0]) ? $atts[0] : null;
+			$field = isset( $atts[0] ) ? $atts[0] : null;
 		}
 
 		switch ( $field ) {
@@ -146,7 +156,7 @@ class CCS_BP_Member {
 				return $current_user->user_url;
 				break;
 			case 'avatar':
-				return get_avatar( $current_user->ID, !empty($size) ? $size : 96);
+				return get_avatar( $current_user->ID, ! empty( $size ) ? $size : 96 );
 				break;
 			case 'fullname':
 				return $current_user->display_name;
@@ -155,12 +165,42 @@ class CCS_BP_Member {
 				return strval( count_user_posts( $current_user->ID ) );
 				break;
 			case 'role':
-				return rtrim(implode(',',array_map('ucwords', $current_user->roles)),',');
+				return rtrim( implode( ',', array_map( 'ucwords', $current_user->roles ) ), ',' );
 				break;
 			default:
 				return get_user_meta( $current_user->ID, $field, true );
 				break;
 		}
+
+	}
+
+	/*========================================================================
+	 *
+	 * Get users by xprofile data.
+	 *
+	 *=======================================================================*/
+	public function get_users_by_xprofile_data( $xprofile_fields ) {
+
+		global $wpdb;
+
+		if ( ! $xprofile_fields ) {
+			return false;
+		}
+
+		foreach ( $xprofile_fields as $key => $value ) {
+			if ( $value ) {
+				$key = xprofile_get_field_id_from_name( $key );
+				$field_ids[] = "'" . esc_sql( $key ) . "'";
+				$values[] = "'" . esc_sql( $value ) . "'";
+			}
+		}
+
+		$field_ids = implode( ',', $field_ids );
+		$values = implode( ',', $values );
+
+		// Get user IDs based on an xprofile field
+		$ids = $wpdb->get_col( "SELECT user_id FROM {$wpdb->prefix}bp_xprofile_data WHERE field_id in ({$field_ids}) AND value in ({$values})" );
+		return array_unique( $ids );
 
 	}
 
